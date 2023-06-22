227718c366b1c895e6dd67ea56d9440a331ed029
keita-yamaoka <keita.yamaoka.seattle@gmail.com>
2023-06-22 22:33:01 +0900

    時間によってあいさつの内容が変わる修正追加

--- src/controller/SampleController.java
+++ src/controller/SampleController.java
@@ -5,11 +5,11 @@ import service.impl.SampleServiceimpl;
 
 public class SampleController {
 
-
+// この処理が動いたら取得したaisatsuの値が出力される
 	public static void main(String args[]) {
 		SampleService test = new SampleServiceimpl();
-		String hoge = test.callResponse();
-		System.out.println(hoge);
+		String aisatsu = test.callResponse();
+		System.out.println(aisatsu);
 	}
 
 };
